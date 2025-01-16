0bcfb51dac1a4e60b2b677c062735244893cc362
marioyordanoff <maimunata38@gmail.com>
2025-01-16 21:50:41 +0200

    commit, push to origin

--- agent.py
+++ agent.py
@@ -8,14 +8,14 @@ dotenv.load_dotenv()
 
 llm = OpenAI(model="gpt-4o-mini")
 
-composio_toolset = ComposioToolSet(api_key="85zq31ck6okzfoqknx89rk")
+composio_toolset = ComposioToolSet()
 tools = composio_toolset.get_tools(actions=['HACKERNEWS_SEARCH_POSTS'])
 
 prefix_messages = [
     ChatMessage(
         role="system",
         content=(
-            "You are now a integration agent, and what  ever you are requested, you will try to execute utilizing your tools."
+            "You are now a integration agent, and whatever you are requested, you will try to execute utilizing your tools."
         ),
     )
 ]
@@ -29,7 +29,7 @@ agent = FunctionCallingAgentWorker(
     verbose=True,
 ).as_agent()
 
-response = agent.chat("Search through hackernews and find me 3 posts where people discuss ideas about building AI agents or agentic AI. Go back in time if you cannot find posts from today.")
+response = agent.chat("Search through hackernews and find me only 1 post where people discuss ideas about building AI agents or agentic AI. Go back in time if you cannot find posts from today.")
 print("Response:", response)
 
 dotenv.load_dotenv()
